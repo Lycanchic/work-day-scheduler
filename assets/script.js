e9aba0d7735e28f7c8d418aeb80a446e51b663b8
Marie P <map40j@gmail.com>
2022-11-16 20:59:26 -0800

    connected the CSS and JS files to the HTML

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var today = new Date();
+var day = today.getDay();
+var dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+console.log("Today is : " + dayList[day] + ".");
