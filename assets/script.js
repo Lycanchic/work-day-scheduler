d25fe8b70ba75f95ac28ff7fc33482109dbe2af7
Marie P <map40j@gmail.com>
2022-11-19 20:33:27 -0800

    added the current date to the Jumbotron and the grid for the day's timeblocks.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,68 @@
+// Current date displayed at the top of the page
 var today = new Date();
 var day = today.getDay();
-var dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-console.log("Today is : " + dayList[day] + ".");
+var dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+document.querySelector(".day").innerHTML = "Today is : " + dayList[day];
+
+console.log("Today is : " + dayList[day]);
+
+// Display today's day and date
+var reformatDate = moment("11/18/22", "MM-DD-YY").format("dddd, MMMM Do YYYY, h:mm:ss a");
+$("#3a").text(reformatDate);
+
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+   
+    function timeTracker() {
+        //get current number of hours.
+        var timeNow = moment().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // To check the time and add the classes for background indicators
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    // Get item from local storage if any
+    $("#hour0800 .description").val(localStorage.getItem("hour0800"));
+    $("#hour0900 .description").val(localStorage.getItem("hour0900"));
+    $("#hour1000 .description").val(localStorage.getItem("hour10"));
+    $("#hour1100 .description").val(localStorage.getItem("hour1100"));
+    $("#hour1200 .description").val(localStorage.getItem("hour1200"));
+    $("#hour1300 .description").val(localStorage.getItem("hour1300"));
+    $("#hour1400 .description").val(localStorage.getItem("hour1400"));
+    $("#hour1500 .description").val(localStorage.getItem("hour1500"));
+    $("#hour1600 .description").val(localStorage.getItem("hour1600"));
+    $("#hour1700 .description").val(localStorage.getItem("hour1700"));
+    $("#hour1800 .description").val(localStorage.getItem("hour1800"));
+
+    timeTracker();
+})
